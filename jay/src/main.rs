63afe6e7e77874d3859b9f10348dbe0991aed26f
Jakob Jordan <jakobjordan@posteo.de>
2022-11-04 19:36:09 +0100

    Simplify routing

--- jay/src/main.rs
+++ jay/src/main.rs
@@ -4,10 +4,16 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use tokio::runtime;
 
-static STATIC_HTML: &str = include_str!("static.html");
-static CV_HTML: &str = include_str!("cv.html");
-static STYLE_CSS: &str = include_str!("style.css");
-static PIC: &'static [u8] = include_bytes!("photo6019329608475848615_crop.jpg");
+static STR_ASSETS: &'static [(&str, &str)] = &[
+    ("/cv.html", include_str!("cv.html")),
+    ("/static.html", include_str!("static.html")),
+    ("/style.css", include_str!("style.css")),
+];
+
+
+static BYTE_ASSETS: &'static [(&str, &'static [u8])] = &[
+    ("/figures/photo6019329608475848615_crop.jpg", include_bytes!("photo6019329608475848615_crop.jpg")),
+];
 
 fn main() {
     let rt = runtime::Builder::new_current_thread()
@@ -18,8 +24,6 @@ fn main() {
 }
 
 async fn run() {
-    // 1) generalize to "assets" which are read at build time
-    // 2) minimal logging
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     let make_svc = make_service_fn(|_conn| async {
@@ -36,12 +40,12 @@ async fn run() {
 }
 
 fn handle(req: Request<Body>) -> Response<Body> {
-    match req.uri().path() {
-        "/static.html" => Response::new(STATIC_HTML.into()),
-        "/cv.html" => Response::new(CV_HTML.into()),
-        "/style.css" => Response::new(STYLE_CSS.into()),
-        "/figures/photo6019329608475848615_crop.jpg" => Response::new(PIC.into()),
-        _ => Response::builder()
+    if let Ok(index) = STR_ASSETS.binary_search_by_key(&req.uri().path(), |&(a, _b)| a) {
+        Response::new(STR_ASSETS[index].1.into())
+    } else if let Ok(index) = BYTE_ASSETS.binary_search_by_key(&req.uri().path(), |&(a, _b)| a) {
+        Response::new(BYTE_ASSETS[index].1.into())
+    } else {
+        Response::builder()
             .status(StatusCode::NOT_FOUND)
             .body(Body::empty())
             .unwrap()
